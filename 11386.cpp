9e117024820b35fae62e8c612fb6187c52fa3db9
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2011-12-28 12:18:56 +0530

    Dec 27, 2011

---
+++ 11386.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+typedef long long int64;
+
+int main() {
+	int64 N;
+	while (scanf("%lld", &N) != EOF) {
+		int64 I = 0, T = 0;
+		map<int64, int64> Map;
+		map<int64, int64>::iterator ii, jj;
+		for (int64 i = 0; i < N; i = i + 1) {
+			scanf("%lld", &I);
+			++Map[I];
+		}
+		for (ii = Map.begin(); ii != Map.end(); ii++) {
+			for (jj = ii; jj != Map.end(); jj++) {
+				int64 A1 = (*ii).first;
+				int64 A2 = (*ii).second;
+				int64 B1 = (*jj).first;
+				int64 B2 = (*jj).second;
+				int64 temp = 0;
+				if (A1 == B1)
+					temp += (A2 * (A2 - 1))/2;
+				else
+					temp += (A2 * B2);
+					
+				if (Map.find(A1 + B1) != Map.end())
+					T += (temp * Map[A1 + B1]);
+			}
+		}
+		printf("%lld\n", T);
+	}
+}
